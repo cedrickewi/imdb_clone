9fe2002cff78f68ceceecc9161180489f0dbc738
cedrickewi <cedrickewi@gmail.com>
2023-12-02 23:29:20 +0100

    install nextjs

---
+++ src/app/head.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function head() {
+  return (
+    <>
+    <title>IMDB CLONE</title>
+    <meta content="width=device-width, initial-scale=1" name='viewport'/>
+    <meta content="Generated b create next app" name='description'/>
+    </>
+  )
+}
+
+export default head
+ 
